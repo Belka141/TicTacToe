08e10eeae66e444485e53d027d4fe30705f0c6b5
Uladislav Bialou <belo4kalala1441@gmail.com>
2022-09-21 13:24:51 +0300

    fixed the problem then pl;ayer choose  row or column <3, also added algoritm to choose  who make move first

--- src/main/java/com/example/tictactoe/TicTacToeConsole/Human.java
+++ src/main/java/com/example/tictactoe/TicTacToeConsole/Human.java
@@ -12,17 +12,24 @@ public class Human extends Player {
         boolean turn = true;
 
         while (turn) {
-            System.out.println("please enter row");
+            System.out.println("please enter row 1-3");
             int row = s.nextInt();
-            System.out.println("please enter col");
+            if (row > 3) {
+                System.out.println("invalid row, try again 1-3");
+                 row = s.nextInt();
+            }
+            System.out.println("please enter col 1-3");
             int col = s.nextInt();
+            if (col > 3) {
+                System.out.println("invalid column, try again ");
+                col = s.nextInt();
+            }
             System.out.print("you entered " + row + " " + col);
             System.out.println();
             if (board[row - 1][col - 1] != "x" && board[row - 1][col - 1] != "o") {
                 board[row - 1][col - 1] = Marker;
                 turn = false;
-            }
-            else {
+            } else {
                 System.out.println("Already Marker here! please guess again!");
             }
         }
--- src/main/java/com/example/tictactoe/TicTacToeConsole/TicTacToeConsole.java
+++ src/main/java/com/example/tictactoe/TicTacToeConsole/TicTacToeConsole.java
@@ -23,14 +23,15 @@ public class TicTacToeConsole {
             System.out.println("please choose your marker");
             System.out.println("type 1 for 'x' or 2 for 'o'");
 
-            if (s.nextInt() == 1) {
+            int a = s.nextInt();
+            if (a == 1) {
                 human.setMarker("x");
                 computer.setMarker("o");
-            } else if (s.nextInt() == 2) {
+            } else if (a == 2) {
                 human.setMarker("o");
                 computer.setMarker("x");
-            }else{
-                System.out.println("invalid commad");
+            } else {
+                System.out.println("invalid command");
             }
 
             int first = (int) (Math.random() * 2);
@@ -60,6 +61,28 @@ public class TicTacToeConsole {
                         System.out.println();
                         if (setBoard.hasWon(setBoard.getBoard())) {
                         }
+                    }
+                }
+            } else if (first == 1) {
+                while (!won) {
+                    computer.takeTurn(setBoard.getBoard(), human);
+                    turns++;
+                    System.out.println();
+                    setBoard.printBoard();
+                    System.out.println();
+                    if (setBoard.hasWon(setBoard.getBoard())) {
+                    }
+                    if (turns == 9) {
+                        won = true;
+                        System.out.println("Its a bore draw!");
+                        break;
+                    }
+                    human.takeTurn(setBoard.getBoard());
+                    turns++;
+                    setBoard.printBoard();
+                    if (setBoard.hasWon(setBoard.getBoard())) {
+                        won = true;
+                        System.out.println("Congrats you won!");
                     }
                 }
             }
